9c92cf39a84c3522e69fb4bcfa193a08ea352a4a
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-04-07 00:51:04 -0500

    Update variable name

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Please take a backup of /var/www/html/WikiName and your MySQL database first!"
-read -p "Enter the name of your wiki: " WIKI
+read -p "Enter the name of your wiki: " WIKI_NAME
 
 echo "Clearing pending jobs"
-cd /var/www/html/$WIKI/maintenance
+cd /var/www/html/$WIKI_NAME/maintenance
 php runJobs.php --quiet --nothrottle
 
 echo "Updating MediaWiki"
@@ -12,10 +12,10 @@ cd /var/www/html/
 wget https://releases.wikimedia.org/mediawiki/1.27/mediawiki-1.27.4.tar.gz
 tar -xvzf mediawiki-1.27.4.tar.gz
 rm -f mediawiki-1.27.4.tar.gz
-cp -R -f mediawiki-1.27.4/* $WIKI
+cp -R -f mediawiki-1.27.4/* $WIKI_NAME
 rm -rf mediawiki-1.27.4
-chown -R www-data:www-data $WIKI
-cd $WIKI/maintenance
+chown -R www-data:www-data $WIKI_NAME
+cd $WIKI_NAME/maintenance
 php update.php
 
 echo "Finished!"
