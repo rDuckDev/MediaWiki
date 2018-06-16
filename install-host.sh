a5c7f180cd011c67716731aa5a7126901b5306de
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-06-15 20:42:01 -0500

    Colorize and clarify output

--- install-host.sh
+++ install-host.sh
@@ -6,50 +6,53 @@ GRN="\033[0;32m"
 ORG="\033[0;33m"
 BLU="\033[0;34m"
 
-echo "Installing LAMP"
+echo -e "${BLU}Installing LAMP stack${NoC}"
+read -p "Press any key to continue..." -n 1 -r
 apt-get install apache2 libapache2-mod-php mysql-server php php-apcu php-cli php-curl php-intl php-mbstring php-mysql php-xml
 
-echo "Installing required software"
+echo -e "${BLU}Installing required software${NoC}"
+read -p "Press any key to continue..." -n 1 -r
 cd /tmp
 apt-get install imagemagick wget zip unzip git nodejs npm pwgen
 wget https://getcomposer.org/installer
 php installer --filename=composer --install-dir=/bin
 
-echo "Securing MySQL"
+echo -e "${BLU}Securing MySQL${NoC}"
+read -p "Press any key to continue..." -n 1 -r
 mysql_secure_installation
 
-echo "How would you like to administer your machine?"
-echo "  1) Cockpit"
-echo "  2) Webmin"
-echo "  3) SSH"
-echo "  4) GUI"
-echo "  5) None"
-read -p "Option: " OPTION
+echo -e "${BLU}How would you like to administer your machine?"
+echo -e "  1) Cockpit"
+echo -e "  2) Webmin"
+echo -e "  3) SSH"
+echo -e "  4) GUI"
+echo -e "  5) None${NoC}"
+read -p "Option: " -n 1 -r
+echo
 
-case $OPTION in
-	1 )	echo "Installing Cockpit"
-		apt-get install cockpit
-		;;
-	2 ) echo "Installing Webmin"
-		cd /tmp
-		wget http://prdownloads.sourceforge.net/webadmin/webmin_1.881_all.deb
-		dpkg --install webmin_1.881_all.deb
-		# dpkg might complain about dependencies,
-		# so fix them and finish installation
-		apt-get -f install
-		rm webmin_1.881_all.deb
-		;;
-	3 ) echo "Installing SSH"
-		apt-get install openssh-server
-		;;
-	4 ) echo "Installing GUI"
-		apt-get install --no-install-recommends ubuntu-desktop
-		apt-get install firefox gedit
-		;;
-	* ) echo ""
-		;;
+case $REPLY in
+  1 ) echo -e "${BLU}Installing Cockpit${NoC}"
+      apt-get install cockpit
+      ;;
+  2 ) echo -e "${BLU}Installing Webmin${NoC}"
+      cd /tmp
+      wget http://prdownloads.sourceforge.net/webadmin/webmin_1.881_all.deb
+      dpkg --install webmin_1.881_all.deb
+      # dpkg might complain about dependencies,
+      # so fix them and finish the installation
+      apt-get -f install
+      rm webmin_1.881_all.deb
+      ;;
+  3 ) echo -e "${BLU}Installing SSH${NoC}"
+      apt-get install openssh-server
+      ;;
+  4 ) echo -e "${BLU}Installing GUI${NoC}"
+      apt-get install --no-install-recommends ubuntu-desktop
+      apt-get install firefox gedit
+      ;;
+  * ) echo -e ""
+      ;;
 esac
 
-echo "Finished!"
-
-read -p "Press enter to continue..." BURN
+echo -e "${GRN}Finished!${NoC}"
+read -p "Press any key to continue..." -n 1 -r
